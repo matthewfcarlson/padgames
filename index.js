750ed3b98ae9a36c6b3c05c3152ece0fa97c07ca
Matthew Carlson <matthewfcarlson@gmail.com>
2018-03-12 13:31:46 -0700

    added socket stuff

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var app = require("express")();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req,res){
     //res.send("<h1>Hello World</h1>");
     res.sendFile(__dirname + "/index.html");
 });
+
+io.on("connection", function(socket){
+    console.log("a user connected");
+    socket.on("chat message",function(msg){
+        io.emit("chat message", msg);
+        //socket.emit("chat message")
+        console.log("message: "+msg);
+    })
+});
+
 http.listen(3000, function (){
     console.log("Listening on 3000");
 });
