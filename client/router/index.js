f9c9cf0b3594f7b69227cfcc520801800fc1d507
Matthew Carlson <macarl@microsoft.com>
2018-03-23 15:00:14 -0700

    changed the router settings

--- client/router/index.js
+++ client/router/index.js
@@ -23,10 +23,7 @@ routes.concat(GameRoutes);
 
 export default new Router({
   routes: routes,
-  hashbang: false,
   //abstract: true,
   history: true,
-  mode: 'history',
-  linkActiveClass: 'active',
-  transitionOnLoad: true,
+  mode: 'history'  
 });
