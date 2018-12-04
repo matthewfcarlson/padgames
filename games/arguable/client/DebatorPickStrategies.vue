cd8300ab814ec870a111526a36e89dc0229d5d47
Matthew Carlson <matthewfcarlson@gmail.com>
2018-12-04 00:49:48 -0800

    fixed another timer bug

--- games/arguable/client/DebatorPickStrategies.vue
+++ games/arguable/client/DebatorPickStrategies.vue
@@ -1,15 +1,21 @@
 <template>
-<div class="container">
+  <div class="container">
     <h2>Here's your strategies you'll be debating:</h2>
     <ul class="list-group list-group-flush">
-      <li v-for="strategy,index in strategies" v-bind:key="index" class="list-group-item list-group-item-action">{{strategy}}</li>
+      <li
+        v-for="strategy,index in strategies"
+        v-bind:key="index"
+        class="list-group-item list-group-item-action"
+      >{{strategy}}</li>
     </ul>
-    <div v-if="!ready">Think about what you want to do with your strategies. You'll have 30 seconds to think about it. The debate will last 2-4 minutes and will end when the moderator ends the debate.</div>
+    <div
+      v-if="!ready"
+    >Think about what you want to do with your strategies. You'll have 30 seconds to think about it. The debate will last 2-4 minutes and will end when the moderator ends the debate.</div>
     <button v-if="!ready" @click="DebatorReady" class="btn btn-success btn-block">
-        <span v-if="!ready">Click when you are ready ({{secondsLeft}})</span>
-        <span v-else>You are ready!</span>
+      <span v-if="!ready">Click when you are ready ({{secondsLeft}})</span>
+      <span v-else>You are ready!</span>
     </button>
-</div>
+  </div>
 </template>
 <script>
 import Vue from "vue";
@@ -61,53 +67,51 @@ export default {
   name: "ModeratorPickTheDebators",
   data() {
     return {
-        strategies : [],
-        ready: false,
-        counter: false,
-        interval: null,
-        timeLeft: 0,
-        secondsLeft: "0:25"
+      strategies: [],
+      ready: false,
+      counter: false,
+      interval: null,
+      timeLeft: 0
     };
   },
   computed: {
-      secondsLeft: function(){
-          var seconds = this.timeLeft;
-          if (seconds < 10) seconds = "0"+seconds;
-          return "0:"+seconds;
-      }
+    secondsLeft: function() {
+      var seconds = this.timeLeft;
+      if (seconds < 10) seconds = "0" + seconds;
+      return "0:" + seconds;
+    }
   },
   methods: {
-    DebatorReady: function(){
+    DebatorReady: function() {
       if (this.ready) return;
       this.ready = true;
       this.$emit("submit");
       clearInterval(this.interval);
     },
     startTimer() {
-        this.interval = setInterval(this.countDown, 1000);
-        this.timeLeft = Math.round(this.timer /1000);
-    },    
+      this.interval = setInterval(this.countDown, 1000);
+      this.timeLeft = Math.round(this.timer / 1000);
+    },
     countDown() {
-        console.log("counting down", this.timeLeft)
-        this.timeLeft -= 1000;
-        if (this.timeLeft <= 0) {
-            this.DebatorReady();
-        }
+      console.log("counting down", this.timeLeft);
+      this.timeLeft -= 1;
+      if (this.timeLeft <= 0) {
+        this.DebatorReady();
+      }
     }
   },
   props: ["timer"],
-  beforeDestroy: function(){
-      clearInterval(this.interval);
+  beforeDestroy: function() {
+    clearInterval(this.interval);
   },
-  mounted: function(){
-    while(this.strategies.length < 2){
-      var randIndex = Math.floor(Math.random()*allStrategies.length);
+  mounted: function() {
+    while (this.strategies.length < 2) {
+      var randIndex = Math.floor(Math.random() * allStrategies.length);
       var strategy = allStrategies[randIndex];
       if (this.strategies.indexOf(strategy) == -1)
         this.strategies.push(strategy);
     }
     this.startTimer();
-  },
-
-}
+  }
+};
 </script>
