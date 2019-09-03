3a4ae483244f0f103500db483b8f9d4b52e8ccc9
Matthew Carlson <matthewfcarlson@gmail.com>
2019-09-03 08:26:18 -0700

    added start of dynamic route system

---
+++ games/passphrase/client/game.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="phasephrase">
+    <NavBar/>
+    <div class="container">
+      <h1>This is an the passphrase game page</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+  }
+})
+export default class PhasePhraseGame extends Vue {}
+</script>
---
+++ games/passphrase/client/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="phasephrase">
+    <NavBar/>
+    <div class="container">
+      <h1>This is an passphrase description page</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {    
+  }
+})
+export default class PhasePhrase extends Vue {}
+</script>
