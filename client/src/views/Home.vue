9cde858fc1234b8e3d43f47a59383a5c2e23a4fc
matthewfcarlson <matthewfcarlson@users.noreply.github.com>
2019-09-09 09:21:12 -0700

    added other sections

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <!-- Games that get your friends and family talking -->
-    <div class="bg-primary escape-gutters text-white ">
+    <div class="bg-primary escape-gutters text-white">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-md-4 col-sm-12">
@@ -29,35 +29,61 @@
             />
           </div>
           <div class="col-md-8 col-sm-12">
-            <br class="d-none d-md-block"/>
+            <br class="d-none d-md-block" />
             <p class="lead font-weight-bold">Games that get your family or friends talking</p>
             <p>Most games work with a central tablet or two that everyone can see and then their phones act as controllers. Some games only require one phone per team.</p>
-            <p> Games are flexible for your situation and some can be played with just phones or just the tablet in the middle.</p>
+            <p>Games are flexible for your situation and some can be played with just phones or just the tablet in the middle.</p>
             <p>Since it is hosted on the internet, people can join from anywhere! You’ll need to be able to communicate with your teammates, so a phone call or discord is recommended.</p>
-            <br class="d-none d-md-block"/>
+            <br class="d-none d-md-block" />
           </div>
         </div>
       </div>
     </div>
     <!-- Mailing list signup -->
-    <NewsLetter/>
-    <div>
-      Want to be updated on everything going on?
-      Signup to our newsletter to hear the latest news! We promise few emails and we'll never give your email away.
-      <input type="email" class="input-control" placeholder="your email here :)"/>
+    <NewsLetter />
+    <div class="container">
+      <div class>Want to be updated on everything going on?</div>Signup to our newsletter to hear the latest news! We promise few emails and we'll never give your email away.
+      <input
+        type="email"
+        class="input-control"
+        placeholder="your email here :)"
+      />
     </div>
     <!-- Game choices -->
-    <div>
-      A wide choice of games
-      Ever been sitting around with your family or friends in the room, all on your phones, not talking? Games that are designed to help you talk and have fun together.
+    <div class="container">
+      <div class="row">
+        <div class="col col-sm-12 lead">A wide choice of games</div>
+        <div class="col col-sm-12">
+          Ever been sitting around with your family or friends in the room, all on your phones, not talking? Games that are designed to help you talk and have fun together.
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-xs-12 col br-5 bg-primary">
+          Game 1
+        </div>
+      </div>
     </div>
     <!-- cost -->
-    <div>
-      What does it cost?
-      Free to join
-      $1 to host
-      Joining games will always be free. Unlocking a game for $1 will give you unlimited hosts. Or you can unlock all the games for one low price of $X.99
-    </div>    
+    <div class="bg-gray escape-gutters">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col text-center">
+            <h3>What does it cost?</h3>
+            <p>Free to join</p>
+            <p>$1 to host</p>
+            <br/>
+            <p>Joining games will always be free. Unlocking a game for $1 will give you unlimited hosts. Or you can unlock all the games for one low price of $X.99</p>
+          </div>
+          <div class="col">
+            <img
+              src="/public/default_sushi.png"
+              class="img-fluid img-center"
+              alt="family playing together"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- FAQ -->
     <div>FAQ</div>
   </div>
@@ -66,7 +92,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import NavBar from "../components/NavBar.vue"; // @ is an alias to /src
-import NewsLetter from '../components/NewsLetter.vue';
+import NewsLetter from "../components/NewsLetter.vue";
 
 @Component({
   components: {
