087c09a7578dd3cfe159d37fcfede2144fa9846f
matthewfcarlson <matthewfcarlson@users.noreply.github.com>
2019-10-01 10:50:30 -0700

    tweaks to newsletter

--- client/src/components/NewsLetter.vue
+++ client/src/components/NewsLetter.vue
@@ -1,31 +1,42 @@
 <template>
   <div>
     <p class="lead mb-0">Want to be updated on everything going on?</p>
-    <p>
-      Signup for our our monthlyish newsletter! We'll never give your email away.
-      <br />
-    </p>
-    <form
-      action="https://app.mailerlite.com/webforms/submit/u6p7v6"
-      data-code="u6p7v6"
-      method="post"
-      target="_blank"
-    >
-      <div class="input-group">
-        <input
-          type="email"
-          class="form-control"
-          data-inputmask
-          name="fields[email]"
-          value
-          placeholder="Email"
-          aria-label="Recipient's email"
-        />
-        <div class="input-group-append">
-          <button type="submit" class="btn btn-primary btn-sm">Subscribe</button>
+    <div v-if="!subscribed">
+      <p>
+        Signup for our our monthlyish newsletter! We'll never give your email away.
+        <br />
+      </p>
+      <form
+        action="https://app.mailerlite.com/webforms/submit/u6p7v6"
+        data-code="u6p7v6"
+        method="post"
+        target="_blank"
+        @submit="subscribeToNewsLetter"
+      >
+        <div class="input-group">
+          <input
+            type="email"
+            class="form-control"
+            data-inputmask
+            name="fields[email]"
+            value
+            placeholder="Email"
+            aria-label="Recipient's email"
+          />
+          <div class="input-group-append">
+            <button type="submit" class="btn btn-primary btn-sm">Subscribe</button>
+          </div>
         </div>
+      </form>
+    </div>
+    <div v-else-if="subscribing">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
       </div>
-    </form>
+      <p>Subscribing you now</p>
+    </div>
+    <div v-else>Thanks for subscribing</div>
+
     <!--div class="ml-form-align-center">
       <div class="ml-form-embedWrapper embedForm">
         <div class="ml-form-embedBody ml-form-embedBodyHorizontal row-form">
@@ -66,5 +77,16 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class NewsLetter extends Vue {}
+export default class NewsLetter extends Vue {
+  subscribed = false;
+  subscribing = false;
+
+  subscribeToNewsLetter(e: Event) {
+    console.log(e);
+    e.preventDefault();
+    this.subscribing = true;
+
+    // TODO send AJAX request to mail
+  }
+}
 </script>
