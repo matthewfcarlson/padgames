5f744ba842928a56d5a798bf92f35f0df16a2ba6
Matthew Carlson <matthewfcarlson@gmail.com>
2019-11-17 10:19:24 -0800

    updated route information

--- games/passphrase/client/index.vue
+++ games/passphrase/client/index.vue
@@ -2,7 +2,17 @@
   <div class="phasephrase">
     <NavBar/>
     <div class="container">
-      <h1>This is an passphrase description page</h1>
+      <div class="row">
+      <div class="col">
+        <img src="./assets/passphrase_logo.svg" alt="game logo for the game" class="game-logo" />
+      </div>
+      <div>
+        <h1>PassPhrase</h1>
+        <p>Pass phrase</p>
+        <p>TODO: tutorial here? </p>
+      </div>
+      <router-link tag="button" :to="passphrase_game" class="btn btn-success">Host a Game</router-link>
+      </div>
       {{ color }}
     </div>
   </div>
